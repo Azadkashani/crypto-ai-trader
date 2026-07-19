f57f10175e2a6543aac2a6555c96c94e82b5dce0
Masoud Kashani <salar0033@gmail.com>
2026-07-19 20:12:05 +0330

    Update report.py

--- crypto_ai_bot/report.py
+++ crypto_ai_bot/report.py
@@ -1,1 +1,44 @@
+"""
+Crypto AI Bot v4
+Report Engine
+"""
 
+import pandas as pd
+
+
+class ReportEngine:
+
+    @staticmethod
+    def show(results):
+
+        if len(results) == 0:
+            print("No Data")
+            return
+
+        table = pd.DataFrame(results)
+
+        table = table.sort_values(
+            by="Score",
+            ascending=False
+        )
+
+        print("\n")
+        print("=" * 100)
+        print("CRYPTO AI BOT MARKET SCANNER")
+        print("=" * 100)
+
+        print(
+            table[
+                [
+                    "Symbol",
+                    "Price",
+                    "Trend",
+                    "Strength",
+                    "RSI",
+                    "Score",
+                    "Action"
+                ]
+            ].to_string(index=False)
+        )
+
+        print("=" * 100)
