00a10b4c149f2057cd9f6ebb04a924fec9906c28
Masoud Kashani <salar0033@gmail.com>
2026-07-19 20:13:54 +0330

    Update main.py

--- crypto_ai_bot/main.py
+++ crypto_ai_bot/main.py
@@ -1,1 +1,23 @@
+"""
+Crypto AI Bot v4
+Main Program
+"""
 
+from scanner import MarketScanner
+from report import ReportEngine
+
+
+def main():
+
+    print("Starting Crypto AI Bot...")
+    print("Scanning Market...\n")
+
+    scanner = MarketScanner()
+
+    results = scanner.scan()
+
+    ReportEngine.show(results)
+
+
+if __name__ == "__main__":
+    main()
