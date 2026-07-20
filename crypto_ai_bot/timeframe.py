a3e8cb92d7dc9015fbacf5a0ba2a740f6442542c
Masoud Kashani <salar0033@gmail.com>
2026-07-20 04:40:41 +0330

    Update timeframe.py

--- crypto_ai_bot/timeframe.py
+++ crypto_ai_bot/timeframe.py
@@ -1,1 +1,37 @@
+"""
+Crypto AI Bot v5.7
+Timeframe Manager
+"""
 
+from data import MarketData
+
+
+class TimeframeManager:
+
+
+    def __init__(self):
+
+        self.data = MarketData()
+
+
+
+    def get_timeframe_data(self, symbol, timeframe):
+
+        return self.data.exchange.fetch_ohlcv(
+            symbol,
+            timeframe=timeframe,
+            limit=200
+        )
+
+
+
+    def get_dataframe(self, symbol, timeframe):
+
+        candles = self.get_timeframe_data(
+            symbol,
+            timeframe
+        )
+
+        return self.data.to_dataframe(
+            candles
+        )
