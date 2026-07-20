590df2f7ceee676f41e7e1e44810a974c97f131a
Masoud Kashani <salar0033@gmail.com>
2026-07-20 20:08:03 +0330

    Update mtf_engine.py

--- crypto_ai_bot/mtf_engine.py
+++ crypto_ai_bot/mtf_engine.py
@@ -1,108 +1,48 @@
 """
-Crypto AI Bot v5.7
+Crypto AI Bot v5.6
 Multi Timeframe Engine
 """
 
-
-from timeframe import (
-    TIMEFRAME_WEIGHT
-)
+from timeframe import TIMEFRAME_WEIGHT
 
 
 class MTFEngine:
 
-
     @staticmethod
     def analyze(results):
-
         """
-        Example:
+        results example:
 
         {
             "15m": "Bullish",
             "1h": "Bullish",
             "4h": "Bearish"
         }
-
         """
 
-        bullish_score = 0
-        bearish_score = 0
-
-
-        details = {}
-
-
+        bullish = 0.0
+        bearish = 0.0
 
         for tf, trend in results.items():
 
-
             weight = TIMEFRAME_WEIGHT.get(tf, 0)
 
-
-            details[tf] = trend
-
-
             if trend == "Bullish":
-
-                bullish_score += weight
-
+                bullish += weight
 
             elif trend == "Bearish":
+                bearish += weight
 
-                bearish_score += weight
+        if bullish >= 0.7:
+            return "Strong Bullish"
 
+        elif bearish >= 0.7:
+            return "Strong Bearish"
 
+        elif bullish > bearish:
+            return "Bullish"
 
-        net_score = round(
-            (bullish_score - bearish_score) * 100,
-            2
-        )
+        elif bearish > bullish:
+            return "Bearish"
 
-
-
-        if bullish_score >= 0.7:
-
-            signal = "Strong Bullish"
-
-
-        elif bearish_score >= 0.7:
-
-            signal = "Strong Bearish"
-
-
-        elif bullish_score > bearish_score:
-
-            signal = "Bullish"
-
-
-        elif bearish_score > bullish_score:
-
-            signal = "Bearish"
-
-
-        else:
-
-            signal = "Neutral"
-
-
-
-        return {
-
-            "signal": signal,
-
-            "bullish": round(
-                bullish_score * 100,
-                2
-            ),
-
-            "bearish": round(
-                bearish_score * 100,
-                2
-            ),
-
-            "score": net_score,
-
-            "details": details
-
-        }
+        return "Neutral"
